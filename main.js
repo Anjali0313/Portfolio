c1985da7e434f43e1a4643d81da68bb091fd03b5
Anjali0313 <anjali03132001@gmail.com>
2025-04-20 00:05:37 +0530

    render skills and qualification

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function renderContainer(){
+import portfolioData from './modules/data.js';
+function renderContainer(item) {
   const app = document.querySelector('#app');
-  app.innerHTML =`<div class="sidebar">
+  app.innerHTML = `<div class="sidebar">
   <div></div>
   <div>About</div>
   <div>My Skills</div>
+   <div>Qualification</div>
   <div>Resume</div>
   <div>Contact</div>
   </div>
@@ -14,17 +16,58 @@ function renderContainer(){
   <br>
   <p>&lt;h1&gt;</p>
   <h1>Hi,<br>
-  I'M  Anjali<br>
-  Front-End developer </h1>
+  I'M  ${item.name}<br>
+  ${item.title} </h1>
   <p>&lt;/h1&gt;</p>
-  <p>&lt;p&gt;<br>Front End Developer / wordpress<br>&lt;p&gt;</p>
+  <p class="p">&lt;p&gt;</p>
+  <p class="p">${item.title} / wordpress</p>
+  <p class="p">&lt;/p&gt;</p>
   <button>Contact me</button>
   </div>
-  <div class"Mypotfolio">
+  <div class="mypotfolio">
   <p>&lt;h2&gt;</p>
   <h1>My Portfolio</h1>
   <p>&lt;/h2&gt;</p>
+   <p class="p">&lt;p&gt;</p>
+  <p class="p">&lt;/p&gt;</p>
+  </div>
+  <div class="Skills">
+  <p>&lt;h2&gt;</p>
+  <h1>Skills</h1>
+  <p>&lt;/h2&gt;</p>
+   <p class="p">&lt;p&gt;</p>
+   <div class="skills">
+          ${item.Skills.map(skill => `
+            <div class="skill"><img src="${skill}" alt="skill icon" /></div>
+          `).join('')}
+        </div>
+         <p class="p">&lt;/p&gt;</p>
+   </div>
+   <div class="Qualification">
+  <p>&lt;h2&gt;</p>
+  <h1>Qualification</h1>
+  <p>&lt;/h2&gt;</p> 
+   <p class="p">&lt;p&gt;</p>
+    <div class="qualification-boxes">
+    ${item.qualifications.map(q => `
+      <div class="qualification-card">
+        <span class="short-title">${getShortForm(q.title)}</span>
+        <div class="details">
+        <p> ${q.title}</p>
+          <p>${q.institution}</p>
+          <p>${q.year}</p>
+        </div>
+      </div>
+    `).join('')}
+    </div>
+     <p class="p">&lt;/p&gt;</p>
   </div>
   </div>`
 }
-renderContainer()
+renderContainer(portfolioData)
+function getShortForm(title) {
+  if (title.includes("MCA")) return "MCA";
+  if (title.includes("B.Sc")) return "B.Sc";
+  if (title.includes("12th")) return "12th";
+  return title;
+}
---
+++ modules/data.js
@@ -0,0 +1,44 @@
+ const portfolioData ={
+    name: "Anjali",
+    title:"Front-End Developer",
+    Skills:[" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu8IaYl3q9jl6N1QWTNYIAHH9nCX64xrLTuQ&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPixDGHHN5VI-ZGO_Uq-_m7MULGn9aMkfFHCIYK2ohxlBXnjpVHqHUs9YimwdNmgkem4&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMvId3MgmE1uQFfCI5R0qxDfix_kb5b1ttpg&s",
+        "https://zerovaega.com/site/assets/files/1425/react-icon_svg.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUBwhMOxO7xHV9OqhDZ585TTapui1-kcj3dA&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbQW0cN1w6qDSzbivb_yzKBcA-e7EXLyCfIQ&s",
+        "https://altcampus.com/images/icons/course/git.svg",
+    ],
+    qualifications: [
+        {
+          title: "Master of Computer Applications (MCA)",
+          institution: "AKTU",
+          year: "2024"
+        },
+        {
+          title: "Bachelor of Science  (B.Sc. CS)",
+          institution: "MJPU",
+          year: "2022"
+        },
+        {
+          title: "12th (Science - PCM)",
+          institution: "CBSE Board",
+          year: "2019"
+        },
+        {
+            title: "10th",
+            institution: "CBSE Board",
+            year: "2017"
+          }
+      ],
+
+    
+};
+export default portfolioData
+//HTMl = https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu8IaYl3q9jl6N1QWTNYIAHH9nCX64xrLTuQ&s
+//CSS = https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gbyrDKs8qY4PPXh1xYHN9AZu0dbMYZrLIw&s
+//js = https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH4hMyi_IAZlvnvxD0HC0x7RXI11LdH8oRCq3zMWVOD0xqpTYmKyzPrBbJms9fdaiHH-Q&usqp=CAU
+// github = https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUBwhMOxO7xHV9OqhDZ585TTapui1-kcj3dA&s
+// vit =https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbQW0cN1w6qDSzbivb_yzKBcA-e7EXLyCfIQ&s
+// git = https://altcampus.com/images/icons/course/git.svg
+// rect = https://zerovaega.com/site/assets/files/1425/react-icon_svg.png
